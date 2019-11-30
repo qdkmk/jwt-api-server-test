bdff3a82be91daa95062d3d1b4911daa0cf4ef94
qdkmk <qdokuhon@gmail.com>
2019-11-30 17:27:41 +0900

    knock setting

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,2 +1,6 @@
-class UserTokenController < Knock::AuthTokenController
+module Api
+  module V1
+    class UserTokenController < Knock::AuthTokenController
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post 'user_token' => 'user_token#create'
+    end
+  end
 end
