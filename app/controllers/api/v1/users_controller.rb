0a39d4c50d2f9891ee9dbdcd71d1e1c96587cd46
qdkmk <qdokuhon@gmail.com>
2019-11-30 18:23:53 +0900

    success curl create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,33 @@ class Api::V1::UsersController < ApplicationController
     render json: { status: 200, user: response_fields(current_user.to_json) }
   end
 
+  def create
+    @user = User.new(users_params)
+
+    if @user.name.blank?
+      response_bad_request
+    else
+      if User.exists?(email: @user.email)
+        response_conflict(:user)
+      else
+        if @user.save
+          response_success(:user, :create)
+        else
+          response_internal_server_error
+        end
+      end
+    end
+  end
+
+  def logout
+    render json: { status: 200 }
+  end
+
   private
+
+  def users_params
+    params.permit(:name, :email, :password)
+   end
 
   def response_fields(user_json)
     user_parse = JSON.parse(user_json)
