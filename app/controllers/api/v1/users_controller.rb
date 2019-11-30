64071eb132c2960814b23701391b2ba045fe14ba
qdkmk <qdokuhon@gmail.com>
2019-11-30 17:47:13 +0900

    success curl

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user, except: %i[create logout]
+  def index
+    render json: { status: 200, user: response_fields(current_user.to_json) }
+  end
+
+  private
+
+  def response_fields(user_json)
+    user_parse = JSON.parse(user_json)
+    user_parse.except('created_at', 'updated_at', 'password', 'password_digest')
+  end
 end
